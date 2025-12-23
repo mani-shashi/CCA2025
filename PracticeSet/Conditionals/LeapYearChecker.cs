786d42e0c75615465960c4507fe73eb23aa9ee1d
mani-shashi <shashi2043@gmail.com>
2025-12-23 09:59:01 +0530

    updated

--- PracticeSet/Conditionals/LeapYearChecker.cs
+++ PracticeSet/Conditionals/LeapYearChecker.cs
@@ -14,7 +14,21 @@ namespace PracticeQuestions
         /// </returns>
         public static bool IsLeapYear(int year)
         {
-            return year % 400 == 0;
+            
+            if (year % 4 == 0 )
+            {
+                if (year % 100 != 0)
+                {
+                    return true;
+                } 
+                else if (year % 400 == 0)
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+            
         }
     }
 }
--- PracticeSet/Conditionals/QuadrantFinder.cs
+++ PracticeSet/Conditionals/QuadrantFinder.cs
@@ -2,5 +2,39 @@ using System;
 
 namespace PracticeQuestions
 {
+    public class QuadrantFinder
+    {
+        /// <summary>
+        /// Method to find Quadrant of a given (x,y) axis
+        /// </summary>
+        /// <param name="x">X Axis position as Int</param>
+        /// <param name="y">Y Axis position as Int</param>
+        /// <returns>
+        /// Quadrant as String
+        /// </returns>
+        public string FindQuadrant(int x, int y)
+        {
+            string Quadrant="";
+            if (x == 0 && y == 0)
+            {
+                Quadrant="Centre";
+            } else if (x > 0 && y > 0)
+            {
+                Quadrant = "1st Quadrant";
+            } else if (x < 0 && y > 0)
+            {
+                Quadrant = "2nd Quadrant";
+            } else if (x < 0 && y < 0)
+            {
+                Quadrant = "3rd Quadrant";
+            }
+            else
+            {
+                Quadrant = "4th Quadrant";
+            }
+            
+            return Quadrant;
+        }
+    }
     
 }
